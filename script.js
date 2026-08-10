665a5a21e2a67df3c56fee00522043b84d4116af
Clarence Navelgas <151995847+crencenav@users.noreply.github.com>
2026-08-10 18:01:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const badge = document.querySelector('.badge');
+if (badge) {
+  badge.addEventListener('click', () => {
+    alert('Burrito says hi! Welcome to the Cheesy Cake web experience.');
+  });
+}
